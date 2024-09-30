eeba44c8e990a18ca55fe092351829781f33ad2e
BreweryIS <moogle.fanfare@gmail.com>
2024-09-30 19:25:47 +0700

    time15.03

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { FaRegHeart } from "react-icons/fa";
 
 const Navbar = () => {
   const navitems = [
@@ -21,20 +24,23 @@ const Navbar = () => {
   ];
   return (
     <div className="w-full flex justify-center py-4 items-center">
-      <div className="w-10/12 flex justify-center px-2 items-center">
+      <div className="w-10/12 flex justify-center py-4 px-2 items-center">
         <div className="flex-1">
           <h1 className="logo font-bold text-2xl">Furniture</h1>
         </div>
-        <div className="flex-1">
-          <ul>
-            {navaitems.map((item, index) => (
+        <div className="flex-1 flex justify-center items-center">
+          <ul className="flex gap-2 justify-center items-center">
+            {navitems.map((item, index) => (
               <li key={index}>
                 <Link to={item.path}>{item.label}</Link>
               </li>
             ))}
           </ul>
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1 flex text-xl gap-2 justify-end items-center">
+          <FaRegHeart />
+          <RiShoppingCartLine />
+        </div>
       </div>
     </div>
   );
