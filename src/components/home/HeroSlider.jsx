408ccae593881856e72109da8d7b6f83efc52a30
BreweryIS <moogle.fanfare@gmail.com>
2024-10-02 20:32:45 +0700

    time45.05

---
+++ src/components/home/HeroSlider.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination, Navigation } from "swiper/modules";
+import { useState } from "react";
+
+import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+import { Link } from "react-router-dom";
+
+const HeroSlider = () => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    fetch("./img/Hero_Slider/slider.json")
+      .then((response) => response.json())
+      .then((data) => setSlides(data))
+      .catch((error) => console.log(`Error:`, error));
+  }, []);
+
+  return (
+    <div className="w-full">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={0}
+        loop={true}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        // navigation={true}
+        // modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {slides.heroSlider &&
+          slides.heroSlider.map((slide) => (
+            <SwiperSlide key={slide.id} className="max-h-[90vh] relative">
+              <img src={slide.image} alt={`Slide${slide.id}`} />
+              <div className="text_container absolute top-[30%] text-center w-full h-full">
+                <h1 className="font-bold text-5xl text-slate-800">
+                  {slide.title}
+                </h1>
+                <p className="text-sm italic py-3 text-gray-100">
+                  {slide.subtitle}
+                </p>
+
+                <Link to={slide.link}>
+                  <button className="font-bold bg-lime-400 py-3 px-5 text-sm hover:bg-lime-600 transition ease-in duration-200">{slide.buttonText}</button>
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default HeroSlider;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import HeroSlider from "../components/home/HeroSlider";
+import Catagories from "../components/home/Catagories";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="w-full">
+        <HeroSlider />
+      </div>
+      <div className="w-11/12">
+      <div className="title_container">
+        <h1 className="title font-bold">
+          Find Your Style:Furniture Categories
+        </h1>
+      </div>
+        <Catagories />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
